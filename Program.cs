04fff5faab13a0a03fd72f6ac11192b5685aec2b
Nicolás Rufino <nicolasrufino@Mateos-MacBook-Pro.local>
2024-04-05 20:35:43 -0600

    1st commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿public class Program
+{
+    static void Main()
+    {
+        Welcome();
+    }
+    static void Welcome()
+    {
+        Console.Clear();
+        Console.WriteLine("WELCOME TO SNOW EXPERIENCE");
+        Thread.Sleep(2000);
+        Console.Clear();
+        // string[] snow = File.ReadAllLines("snowWelcome.txt"); 
+        // Console.WriteLine($"{snow}");
+        if (File.Exists("snowWelcome.txt"))
+        {
+            string[] snow = File.ReadAllLines("snowWelcome.txt");
+            Console.WriteLine(snow);
+            foreach(var i in snow){
+                Console.WriteLine(i);
+            }
+        }
+        Thread.Sleep(2000);
+    }
+}
